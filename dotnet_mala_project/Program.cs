c4b7fb89104417da9959e00776159f2493f7c5ce
Ko Htut <info@kohtut.me>
2023-11-25 15:39:51 +0700

    fix : project setup

--- dotnet_mala_project/Program.cs
+++ dotnet_mala_project/Program.cs
@@ -1,9 +1,126 @@
-﻿namespace dotNet{
+﻿using System;
+using ConsoleTables;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MenuDrivenCLI
+{
     class Program
     {
+
+        // Main
         static void Main(string[] args)
         {
-            Console.WriteLine("Khant Nyar");
+            Console.WriteLine("Welcome to the DotNet-MaLa Menu CLI!");
+            Console.WriteLine("Choose an option:");
         }
+
+        // dine in
+        static void DineIn()
+        {
+            DisplayMenu();
+        }
+
+        // take away
+        static void TakeAway()
+        {
+            DisplayMenu();
+        }
+
+        // menu
+        static void DisplayMenu()
+        {
+            bool exitRequested = false;
+            
+        }
+
+        // single 
+        static void SingleMeuTable()
+        {
+            //
+        }
+
+        // couple
+        static void DisplayMenuTable()
+        {
+            //
+        }
+
+        // custom
+        static void DisplayCustomMenuTable()
+        {
+           //
+        }
+
+        // custom menu opetion
+        static void DisplayCustomMenuOpetion()
+        {
+            //
+        }
+
+        // spicy level
+        static void DisplaySpicyLivelTable()
+        {
+            //
+        }
+
+        // dry or soup
+        static void DispleyDryOrSoupTable()
+        {
+            //
+        }
+
+        // drink
+        static void DisplayDrinkTable()
+        {
+            //
+        }
+
+        // card ( edit & details )
+        static void DisplayCard_Edit_Details()
+        {
+            //
+        }
+
+        // coupon
+        static void DisplayCouponTable()
+        {
+            //
+        }
+
+        // purchace
+        static void Purchace()
+        {
+            //
+        }
+
+        // yes
+        static void Yes()
+        {
+            //
+        }
+
+        // no
+        static void No()
+        {
+            //
+        }
+
+        // invoice 
+        static void DisplayInvoice()
+        {
+            // var table = new ConsoleTable("Option", "Description");
+            // table.AddRow(1, "Option 1");
+            // table.AddRow(2, "Option 2");
+            // table.AddRow(3, "Option 3");
+            // table.AddRow(4, "Exit");
+
+            // table.Write();
+        }
+
     }
 }
